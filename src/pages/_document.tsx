41ca3a1aef0db03c72233a2ba38f45f819a8980e
GBrogio <guilhermebrogio.ps@gmail.com>
2022-09-26 16:01:10 -0300

    chore: add google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,23 @@
+/* eslint-disable @next/next/next-script-for-ga */
+/* eslint-disable react/no-danger */
 import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
     <Html lang="pt-br">
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TBTVNVF');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* FONTS */}
         <link
           rel="preload"
@@ -51,6 +65,13 @@ const Document = () => {
       <body>
         <Main />
         <NextScript />
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBTVNVF" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
       </body>
     </Html>
   );
