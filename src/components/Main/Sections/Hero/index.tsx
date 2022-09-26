c008c32a2fcf4bc997147b1eb25f4c25dda56783
GBrogio <guilhermebrogio.ps@gmail.com>
2022-09-26 19:47:12 -0300

    chore: remove things in mobile screen in hero section

--- src/components/Main/Sections/Hero/index.tsx
+++ src/components/Main/Sections/Hero/index.tsx
@@ -1,21 +1,37 @@
 import { HaiaLogo, KarvasGRMLogo } from '@components/Logos';
+import { useLayoutContext } from '@contexts/Layout/useLayoutContext';
 import { ContainerS } from './styles';
 
 export const Hero = () => {
+  const {
+    currWidth,
+    currTheme: { breakpoints },
+  } = useLayoutContext();
+  const mobileScreen =
+    currWidth >= Number(breakpoints.mobile.replace('px', ''));
+
   return (
     <ContainerS className="mx-w2">
       <div className="slogan">
         <HaiaLogo />
 
-        <h1 className="slogan">
-          ONDE <br />
-          VIVER <br />
-          É MAIS <br />
-          QUE <br />
-          EXISTIR
-        </h1>
+        {mobileScreen && (
+          <h1 className="slogan">
+            ONDE <br />
+            VIVER <br />
+            É MAIS <br />
+            QUE <br />
+            EXISTIR
+          </h1>
+        )}
       </div>
-      <KarvasGRMLogo />
+      <div
+        style={{
+          margin: !mobileScreen ? '0 auto' : '0',
+        }}
+      >
+        <KarvasGRMLogo />
+      </div>
     </ContainerS>
   );
 };
