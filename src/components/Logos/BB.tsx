76bf8c11f05749e38bd28ccb935c6ce45aafd07b
GBrogio <guilhermebrogio.ps@gmail.com>
2022-11-23 18:42:23 -0300

    chore: fix scroll

--- src/components/Logos/BB.tsx
+++ src/components/Logos/BB.tsx
@@ -4,7 +4,7 @@ import BBImage from '../../assets/svg/bb-logo.svg';
 
 export const BBLogo = () => {
   return (
-    <ContainerS variant="bigger">
+    <ContainerS variant="bigger" style={{ width: '80%' }}>
       <BBImage aria-label="Banco do Brasil" />
     </ContainerS>
   );
