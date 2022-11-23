1cafacab5e399cd24b7331ef87294ce0d5da8042
GBrogio <guilhermebrogio.ps@gmail.com>
2022-11-23 16:49:16 -0300

    fix: layout shift

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,84 +1,87 @@
-/* eslint-disable @next/next/next-script-for-ga */
-/* eslint-disable react/no-danger */
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-const Document = () => {
-  return (
-    <Html lang="pt-br">
-      <Head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TBTVNVF');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
+export default class MyDocument extends Document {
+  // permite criação de estilos css em javascript
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-        {/* FONTS */}
-        <link
-          rel="preload"
-          href="/fonts/denike-regular.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/intelo-regular.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/intelo-bold.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/intelo-thin.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/nouvelr-regular.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-        {/* METAS */}
-        <meta charSet="utf-8" />
-        <meta
-          name="description"
-          content="Haia boa vista. Onde viver é mais que existir."
-        />
-        <meta
-          name="facebook-domain-verification"
-          content="ojtlkkiw6afpnrza825mmskj95v25w"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-        {/* Google Tag Manager (noscript) */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBTVNVF" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        />
-        {/* End Google Tag Manager (noscript) */}
-      </body>
-    </Html>
-  );
-};
+      const initialProps = await Document.getInitialProps(ctx);
 
-export default Document;
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  // eslint-disable-next-line no-undef
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          {/* METAS */}
+          <meta charSet="UTF-8" />
+
+          {/* SCRIPTS */}
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charSet="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
+
+          {/* FONTS */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,900;1,400;1,600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap"
+            rel="stylesheet"
+          />
+          {/* LOGO */}
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <body>
+          <Main />
+
+          {/* SCRIPTS */}
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
