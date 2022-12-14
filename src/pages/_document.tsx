abb28006fcd1e6ca59c3ac44157f4e0aaf7c3f9b
haia-web <114267177+haia-web@users.noreply.github.com>
2022-12-14 10:09:56 -0300

    add google tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,6 +46,13 @@ export default class MyDocument extends Document {
         <Head>
           {/* METAS */}
           <meta charSet="UTF-8" />
+          <!-- Google Tag Manager --> 
+          <script>
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-TBTVNVF');
+          </script> 
+          <!-- End Google Tag Manager --> BODY <!-- Google Tag Manager (noscript) --> 
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBTVNVF" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <!-- End Google Tag Manager (noscript) -->
 
           {/* SCRIPTS */}
           <link
