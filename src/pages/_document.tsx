e718c41acbe4fee86a0b781ee85f18abbf85eb84
haia-web <114267177+haia-web@users.noreply.github.com>
2022-12-14 10:15:31 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,7 +48,7 @@ export default class MyDocument extends Document {
           <meta charSet="UTF-8" />
           {/* Google Tag Manager */} 
           <script>
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-TBTVNVF');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id=%27+i+dl';f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-TBTVNVF');
           </script> 
           {/* End Google Tag Manager */} 
           {/* Google Tag Manager (noscript) */}
