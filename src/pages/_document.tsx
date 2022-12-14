597bfd219e58a57cd5cdbdf4b11a4520350e5706
haia-web <114267177+haia-web@users.noreply.github.com>
2022-12-14 10:32:13 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   DocumentContext,
   DocumentInitialProps,
 } from 'next/document';
+import Script from 'next/script';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -47,20 +48,19 @@ export default class MyDocument extends Document {
           {/* METAS */}
           <meta charSet="UTF-8" />
           {/* Google Tag Manager */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function (w,d,s,l,i) {
-                    w[l]=w[l]||[];
-                    w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-                    var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                    j.async=true;
-                    j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                    f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-TBTVNVF')
-                `,
-            }}
-          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              (function (w,d,s,l,i) {
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TBTVNVF')
+            `}
+          </Script>
           {/* End Google Tag Manager */}
 
           {/* SCRIPTS */}
@@ -99,10 +99,10 @@ export default class MyDocument extends Document {
           <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-TBTVNVF"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"`,
+                src="https://www.googletagmanager.com/ns.html?id=GTM-TBTVNVF"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden"`,
             }}
           />
           {/* End Google Tag Manager (noscript) */}
@@ -112,4 +112,3 @@ export default class MyDocument extends Document {
     );
   }
 }
-
